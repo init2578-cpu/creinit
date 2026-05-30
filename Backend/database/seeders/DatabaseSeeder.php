5c8eac25724b7659df2976c662a9753cf82e3406
CodeFire <youssouphbadji.sane@uadb.edu.sn>
2026-05-30 15:30:20 +0000

    Corrictif Add Chapter

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesAndPermissionsSeeder::class,
+            AdminUserSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
